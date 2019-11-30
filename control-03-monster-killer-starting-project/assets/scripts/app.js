da1ba7239593c029a111a90059fa147e86c0ac86
Vansh0007 <Vansh.bhatia9@gmail.com>
2019-11-30 03:23:12 -0500

    Added "Strong attack" and "Heal" Functionality

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,22 +1,53 @@
 let INITIAL_HEALTH = 100;
-let current_HEALTH = INITIAL_HEALTH;
+let CURRENT_HEALTH = INITIAL_HEALTH;
+const STRONG_HEALTH = 17;
+const HEAL_HEALTH = 20;
 const DAMAGE = 10;
 
 adjustHealthBars(INITIAL_HEALTH);
 
-function monsterAttack() {
-  console.log(monsterHealthBar.value + "   " + playerHealthBar.value);
+function handler(mode) {
+  if (mode === "STRONG") {
+    dealMonsterDamage(STRONG_HEALTH);
+
+    dealPlayerDamage(STRONG_HEALTH);
+  } else if (mode === "NORMAL") {
+    dealMonsterDamage(DAMAGE);
+
+    dealPlayerDamage(DAMAGE);
+  } else if (mode === "HEAL") {
+    let healvalue;
+    if (playerHealthBar.value >= INITIAL_HEALTH - HEAL_HEALTH) {
+      alert("Cant Heal More than Your max value");
+      healvalue = INITIAL_HEALTH - playerHealthBar.value;
+      increasePlayerHealth(healvalue);
+    } else {
+      healvalue = HEAL_HEALTH;
+      increasePlayerHealth(healvalue);
+    }
+  }
 
   if (monsterHealthBar.value <= 0 && playerHealthBar.value >= 0) {
     alert("Player Won");
   } else if (monsterHealthBar.value >= 0 && playerHealthBar.value <= 0) {
     alert("Monster Won");
+  } else if (monsterHealthBar.value <= 0 && playerHealthBar.value <= 0) {
+    alert("Match is drawn:");
   }
+}
 
-  dealMonsterDamage(DAMAGE);
+function healPlayer() {
+  handler("HEAL");
+}
 
-  dealPlayerDamage(DAMAGE);
+function strongAttack() {
+  handler("STRONG");
+}
+
+function monsterAttack() {
+  handler("NORMAL");
 }
 
 attackBtn.addEventListener("click", monsterAttack);
-console.log("HI There");
+strongAttackBtn.addEventListener("click", strongAttack);
+healBtn.addEventListener("click", healPlayer);
